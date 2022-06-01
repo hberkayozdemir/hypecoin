88f643e172d6b2d860e17fdc92896f7064f7637d
Hilmi Berkay Ozdemir <berkay.ozdemir@etiya.com>
2022-06-02 01:34:11 +0300

    feat : adding  safe constant  variables for use in application

---
+++ lib/app/core/constants/string_constants.dart
@@ -0,0 +1,8 @@
+/// Class for string constant
+class StringConstant {
+  static const errorMessage = 'Something went wrong';
+  static const hypeBoxKey = 'hype_box_key';
+  static const hypeBox = 'hype_box';
+  static const appConfigDbKey = 'app_config_db_key';
+}
+
